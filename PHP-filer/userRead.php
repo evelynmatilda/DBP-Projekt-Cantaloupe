cae231212e7177fb32835295c8cda3f9053d1603
Hanna Bina Nilsen <bina.99@live.se>
2022-12-07 10:54:54 +0100

    userfiles php

---
+++ PHP-filer/userRead.php
@@ -0,0 +1,36 @@
+<?php 
+ini_set("display_errors", 1);
+
+require_once "userFunctions.php";
+
+$filename = "plants.json";
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod != "GET") {
+    $error = ["error" => "Invalid HTTP method! (Only GET is allowed)"];
+    sendJSON($error, 405);
+}
+
+$plants = [];
+
+if (file_exists($filename)) {
+    $json = file_get_contents($filename);
+    $plants = json_decode($json, true);
+}
+
+if (isset($_GET["id"])) {
+    $id = $_GET["id"];
+
+    foreach ($plants as $plant) {
+        if ($plant["id"] == $id) {
+            sendJSON($plant);
+        }
+    }
+
+    $error = ["error" => "plant not found"];
+    sendJSON($error, 404);
+}
+
+sendJSON($plants);
+
+?>
---
+++ PHP-filer/userUpdate.php
@@ -0,0 +1,55 @@
+<?php 
+ini_set("display_errors", 1);
+
+require_once "userFunctions.php";
+
+$filename = "plants.json";
+$requestMethod = $_SERVER["REQUEST_METHOD"];
+
+if ($requestMethod != "PATCH") {
+    $error = ["error" => "Invalid HTTP method! (Only PATCH is allowed)"];
+    sendJSON($error, 405);
+}
+
+$plants = [];
+
+if (file_exists($filename)) {
+    $json = file_get_contents($filename);
+    $plants = json_decode($json, true);
+}
+
+$requestJSON = file_get_contents("php://input");
+$requestData = json_decode($requestJSON, true);
+
+if (!isset($requestData["id"], $requestData["name"], $requestData["latin"], $requestData["flowers"])) {
+    $error = ["error" => "Bad Request!"];
+    sendJSON($error, 400);
+}
+
+$id = $requestData["id"];
+$newName = $requestData["name"];
+$newLatin = $requestData["latin"];
+$newFlowers = $requestData["flowers"];
+
+if ($newName == "" or $newLatin == "" or $newFlowers == "") {
+    $error = ["error" => "Bad Request!"];
+    sendJSON($error, 400);
+}
+
+foreach ($plants as $index => $plant) {
+    if ($plant["id"] == $id) {
+        $plant["name"] = $newName;
+        $plant["latin"] = $newLatin;
+        $plant["flowers"] = $newFlowers;
+        $plants[$index] = $plant;
+
+        $json = json_encode($plants, JSON_PRETTY_PRINT);
+        file_put_contents($filename, $json);
+        sendJSON($plant);
+    }
+}
+
+$error = ["error" => "Plant not found"];
+sendJSON($error, 404);
+
+?>
