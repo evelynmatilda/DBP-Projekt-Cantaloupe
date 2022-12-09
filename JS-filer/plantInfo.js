935dc21bde494678bc8c6c552688a41004493a4b
Julia Ermansson <ermanssonj@gmail.com>
2022-12-09 13:13:08 +0100

    extra

--- JS-filer/plantInfo.js
+++ JS-filer/plantInfo.js
@@ -6,15 +6,24 @@ function plantInfoDiv(){
     .then(r => r.json())
     .then(resource => {
         resource.forEach(plant => {
+            if(plant.flowers == false){
+            plant.flowers = "Nej";
+           } else{
+            plant.flowers = "Ja";
+           };
            let plantDiv = document.createElement("div");
            plantDiv.classList.add("plantDiv-closed");
            plantDiv.innerHTML = `
            <h2>${plant.name} (${plant.latin})</h2>
            <img src="${plant.img}"></img>
+           <p>Vattnas gånger/veckan? ${plant.waterInt}</p>
+           <p>Blommar den? ${plant.flowers}</p>
+           <p>Hur mycket ljus behöver den? ${plant.sun}</p>
            <p>${plant.info}</p>
            `;
            let wrapper = document.querySelector("#wrapper");
            wrapper.append(plantDiv);
+           
             plantDiv.addEventListener("click", function(){
         if (plantDiv.className == "plantDiv-closed"){
             plantDiv.className = "plantDiv-opened";
