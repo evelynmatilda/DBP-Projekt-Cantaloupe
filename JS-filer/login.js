ff6aa1fc7263c0d537a46367ea40cbf4009bf599
Julia Ermansson <ermanssonj@gmail.com>
2022-12-12 15:21:57 +0100

    login

--- JS-filer/login.js
+++ JS-filer/login.js
@@ -1,2 +1,35 @@
 "use strict";
 
+let registerButton = document.querySelector("#register-button");
+
+registerButton.addEventListener("click", reg)
+
+function reg(event){
+    event.preventDefault();
+
+    var email = document.querySelector("#email").value;
+    var userName = document.querySelector("#username").value;
+    var password = document.querySelector("#password").value;
+
+    addUser(email, userName, password)
+
+};
+
+
+function addUser(email, username, password){
+    
+    const request = new Request("/PHP-filer/userCreate.php",{
+        method:'POST',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+            email: email,
+            username: username,
+            password: password
+        }),
+    });
+
+    fetch(request)
+    .then(r => r.json())
+    .then(console.log)
+
+}
