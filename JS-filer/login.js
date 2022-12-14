93ac282b1ebcb159eed7c78fd984481ea2479a0f
Hanna Bina Nilsen <bina.99@live.se>
2022-12-14 10:46:24 +0100

    login function

--- JS-filer/login.js
+++ JS-filer/login.js
@@ -7,9 +7,9 @@ registerButton.addEventListener("click", reg)
 function reg(event){
     event.preventDefault();
 
-    var email = document.querySelector("#email").value;
-    var userName = document.querySelector("#username").value;
-    var password = document.querySelector("#password").value;
+    let email = document.querySelector("#email").value;
+    let userName = document.querySelector("#username").value;
+    let password = document.querySelector("#password").value;
 
     addUser(email, userName, password)
 
@@ -29,16 +29,41 @@ function addUser(email, username, password){
     });
 
     fetch(request)
-    .then(r => r.json())
-    .then(console.log)
+        .then(r => r.json())
+        .then(console.log)
 
 }
 
+document.querySelector(".login-button").addEventListener("click", logIn);
 
 function logIn(event){
     event.preventDefault();
+    let recUsername = document.querySelector("#login-username").value;
+    let recPassword = document.querySelector("#login-password").value;
 
-    var username = document.querySelector("#login-username").value;
-    var password = document.querySelector("#login-password").value;
+    fetch("/JSON-filer/users.json")
+        .then(r => r.json())
+        .then(resource => {
+            resource.forEach(user => {
+                if (recUsername == user.username && recPassword == user.password) {
+                    window.localStorage.setItem("userId", user.userId)
+                }
+            })
+        })
 
 }
+
+let regButton = document.querySelector(".reg-button");
+
+regButton.addEventListener("click", regUser);
+
+function regUser(event){
+    event.preventDefault();
+
+    let regEmail = document.querySelector("#reg-email").value;
+    let regUsername = document.querySelector("#reg-username").value;
+    let regPassword = document.querySelector("#reg-password").value;
+
+    addUser(regEmail, regUsername, regPassword)
+};
+
