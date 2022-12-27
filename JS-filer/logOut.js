de3fff9882121fcda7342c0d41f640fa9067c1a2
Julia Ermansson <ermanssonj@gmail.com>
2022-12-27 18:33:31 +0100

    logout popup

--- JS-filer/logOut.js
+++ JS-filer/logOut.js
@@ -1,14 +1,31 @@
 function logOut(){
     let logOutButton = document.querySelector("#logOutButton");
+    let confirmLogOut = document.querySelector(".confirmLogOut-closed");
     logOutButton.addEventListener("click", function(){
-        if(window.confirm("Är du säker på att du vill logga ut? Tryck OK för ja och Avbryt för nej") == true){
-             window.localStorage.clear()
-            window.location.href = "/HTML-filer/homePage.html";
+        if(confirmLogOut.className == "confirmLogOut-closed"){
+            confirmLogOut.className = "confirmLogOut-opened";
         } else {
-
+            confirmLogOut.className = "confirmLogOut-closed";
         }
-        
-    })
+        // if(window.confirm("Är du säker på att du vill logga ut? Tryck OK för ja och Avbryt för nej") == true){
+        //      window.localStorage.clear()
+        //     window.location.href = "/HTML-filer/homePage.html";
+        // } else {
+
+        // }
+    });
+    let yesLogOut = document.querySelector("#yesLogOut");
+    let noLogOut = document.querySelector("#noLogOut");
+
+    yesLogOut.addEventListener("click", function(){
+        window.localStorage.clear();
+        window.location.href = "/HTML-filer/homePage.html";
+    });
+
+    noLogOut.addEventListener("click", function(){
+       confirmLogOut.className = "confirmLogOut-closed"
+    });
+
 }
 
 logOut()
