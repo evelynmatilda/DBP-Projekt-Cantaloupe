e30e886afc28712e0b12737c668298bd554b89f3
Hanna Bina Nilsen <bina.99@live.se>
2023-01-06 13:26:18 +0100

    radera konto helt klart

--- JS-filer/deleteAccount.js
+++ JS-filer/deleteAccount.js
@@ -1,11 +1,11 @@
 "use strict";
 
-function deleteAccout () {
+function deleteAccout() {
     let delAccountBtn = document.querySelector("#del-account-button");
     let confirmDelete = document.querySelector(".confirmDelete-closed");
 
-    delAccountBtn.addEventListener("click", function(){
-        if(confirmDelete.className == "confirmDelete-closed"){
+    delAccountBtn.addEventListener("click", function () {
+        if (confirmDelete.className == "confirmDelete-closed") {
             confirmDelete.className = "confirmDelete-opened";
         } else {
             confirmDelete.className = "confirmDelete-closed";
@@ -15,18 +15,64 @@ function deleteAccout () {
     let yesDelete = document.querySelector("#yes-delete");
     let noDelete = document.querySelector("#no-delete");
 
-    noDelete.addEventListener("click", function(){
+    noDelete.addEventListener("click", function () {
         confirmDelete.className = "confirmDelete-closed";
     });
 
-    yesDelete.addEventListener("click", eventDeleteAccount);
+    yesDelete.addEventListener("click", getUsersPlants);
 
 }
 
-function eventDeleteAccount (){
+function getUsersPlants() {
     let id = window.localStorage.getItem("userId");
-    console.log(id)
+    const userrqst = new Request(`/PHP-filer/userRead.php?userId=${id}`);
+
+    fetch(userrqst)
+        .then(r => r.json())
+        .then(resource => {
+            let plants = resource.owns.length;
+            eventDeleteUserplants(plants)
+        });
     
+}
+
+function eventDeleteUserplants(plants) {
+    let id = window.localStorage.getItem("userId");
+    const requests = [];
+
+    const plantrqst = new Request(`/PHP-filer/userPlantRead.php`)
+
+    fetch(plantrqst)
+        .then(r => r.json())
+        .then(userPlants => {
+            userPlants.forEach(userPlant => {
+                if (userPlant.userId == id) {
+                    let userPlantId = userPlant.userPlantId;
+                    const deleteUserPlant = new Request("/PHP-filer/userPlantDelete.php", {
+                        method: 'DELETE',
+                        headers: { "Content-type": "application/json; charset=UTF-8" },
+                        body: JSON.stringify({
+                            userPlantId: userPlantId
+                        }),
+                    });
+
+                    requests.push(deleteUserPlant);
+                    if(requests.length == plants){
+                        const allPromises = requests.map(promise => fetch(promise));
+                        Promise.all(allPromises)
+                            .then(responses => Promise.all(responses.map(r => r.json())))
+                            .then(deleteUserAccount)
+                    }
+                }
+
+            });
+        });
+
+}
+
+function deleteUserAccount() {
+    let id = window.localStorage.getItem("userId");
+
     const deleteRequest = new Request("/PHP-filer/userDelete.php", {
 
         method: 'DELETE',
@@ -39,10 +85,12 @@ function eventDeleteAccount (){
     fetch(deleteRequest)
         .then(r => r.json())
         .then(resource => {
-            console.log(resource)
+            console.log(resource);
             localStorage.clear();
             window.location.href = "/HTML-filer/homePage.html";
         });
 }
+
+
 
 deleteAccout()
